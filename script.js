798c43e6d687784c833cf7dfd7de3ed8092a53c1
vgenij-asiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2021-10-22 10:36:29 +0300

    added styles

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let start = document.querySelector('.start');//кнопка начала
-let stop = document.querySelector('.stop');//кнопка остановки
-let reset = document.querySelector('.reset');//кнопка сброса
+let start = document.querySelector('.start');
+let stop = document.querySelector('.stop');
+let reset = document.querySelector('.reset');
 
-let totalSeconds = 0;
+let seconds = 0;
 let timerId = null;
 
 function startTimer() {
     if(!timerId) {
         timerId = setInterval(() => {                 
-            document.querySelector('.timer').innerHTML = totalSeconds++;
+            document.querySelector('.timer').innerHTML = seconds++;
         }, 1000)
     }
 }
@@ -23,15 +23,16 @@ function stopTimer() {
     // document.querySelector('.timer').innerHTML = 'STOP'    
 }
 
-function resetTimer() { 
-    if(timerId) {       
-        Location.reload();                
+function resetTimer() {
+    stopTimer(); 
+    if(timerId) {        
+        Location.reload();                        
     }
-    totalSeconds = 0;    
+    seconds = 0;    
     document.querySelector('.timer').innerHTML = '';    
 }
 
 
-start.onclick = startTimer//без скобок
-stop.onclick = stopTimer;//без скобок
-reset.onclick = resetTimer;//без скобок
+start.onclick = startTimer
+stop.onclick = stopTimer;
+reset.onclick = resetTimer;
